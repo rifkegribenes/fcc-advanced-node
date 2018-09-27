4a63fc696638eff5c5d14d7031333cf1132331ee
Glitch (emerald-gas) <none>
2018-09-27 05:08:35 +0000

    Import of rifkegribenes/fcc-advanced-node

--- server.js
+++ server.js
@@ -1,39 +1,21 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
-const express = require('express')
-const app = express()
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
+fccTesting(app); //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
-})
+app.route('/')
+  .get((req, res) => {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Listening on port " + process.env.PORT);
+});
