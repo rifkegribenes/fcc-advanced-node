fd13e95ce6388a1f3c7a7d67bea98d064c9def05
rifkegribenes <rifkegribenes@gmail.com>
2018-09-27 05:18:38 +0000

    💷📯 Checkpoint ./package.json:417995/6 ./server.js:417995/205

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const path        = require('path')
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
@@ -11,9 +12,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+
 app.route('/')
   .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.render('index');
   });
 
 app.listen(process.env.PORT || 3000, () => {
