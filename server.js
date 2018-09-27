e6e900fc09343fd71605b16e1747d53c8f737556
rifkegribenes <rifkegribenes@gmail.com>
2018-09-27 05:55:23 +0000

    passport-local

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const mongo = require('mongodb').MongoClient;
 
 const app = express();
@@ -40,23 +41,33 @@ mongo.connect(process.env.DATABASE, (err, db) => {
        done(null, user._id);
      });
       
+    passport.deserializeUser((id, done) => {
+      db.collection('users').findOne(
+        {_id: new ObjectID(id)},
+        (err, doc) => {
+          done(null, doc);
+        }
+      );
+    });
+      
+     passport.use(new LocalStrategy(
+  function(username, password, done) {
+    db.collection('users').findOne({ username: username }, function (err, user) {
+      console.log('User '+ username +' attempted to log in.');
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (password !== user.password) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+)); 
+      
      app.listen(process.env.PORT || 3000, () => {
       console.log("Listening on port " + process.env.PORT);
     }); 
 
 }});
 
-
-
-
-passport.deserializeUser((id, done) => {
-  db.collection('users').findOne(
-    {_id: new ObjectID(id)},
-    (err, doc) => {
-      done(null, doc);
-    }
-  );
-});
 
 app.route('/')
   .get((req, res) => {
